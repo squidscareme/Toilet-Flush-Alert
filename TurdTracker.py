a09c6dc14c48ab7897e91dccd92dfd48e1d33811
squidscareme <fraziersoftware@gmail.com>
2015-05-31 10:05:05 -0400

    Added explanation comments

--- TurdTracker.py
+++ TurdTracker.py
@@ -1,3 +1,27 @@
+'''
+*****************************************************************
+Turd TurdTracker
+
+AUTHOR: Jonathan Frazier
+DATE: May 31, 2015
+
+DESCRIPTION:
+This is a simple program to get my kids interested
+in programming. The idea is to call this code from
+a raspberry pi when a sensor has detected that the
+toilet has flushed.
+
+DEPENDENCIES
+Environment Variables:
+	TURDTRACKER_FACEBOOK_TOKEN
+	TURDTRACKER_FACEBOOK_PAGE_ID
+	TURDTRACKER_FACEBOOK_API_VERSION
+
+This code was written using Linux Mint 17.1 Cinnamon 64-bit  
+and has been designed to run on Linux.
+*****************************************************************
+'''
+
 #!/usr/bin/python
 from TurdTrackerFacebookFeedPost import TurdTrackerFacebookFeedPost
 
